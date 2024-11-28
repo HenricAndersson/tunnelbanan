4ea7babae33a86e0ff2df28bd2d1dda6acbc3164
Henric <Henricx1@hotmail.com>
2024-11-28 18:03:28 +0100

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import { Home } from './components/Home.js';
 
 function App() {
   return (
